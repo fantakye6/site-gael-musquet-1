ce31550389c67fe0487273d4280cf821324e6e14
fantakye6 <fanta.kouyate635@gmail.com>
2025-11-18 12:02:05 +0100

    maj

--- script.js
+++ script.js
@@ -1,4 +1,35 @@
-// Animation des images de la galerie au scroll
+// ===== Animation texte effet hacker =====
+const hackerText = document.getElementById("hacker-text");
+
+const content = `Météorologue de formation et hacker citoyen,
+Gaël Musquet place la technologie au service de l’humain.
+
+À travers des initiatives comme HAND et OpenStreetMap,
+il mobilise la cartographie et les données libres
+pour sauver des vies et redonner du pouvoir aux citoyens.
+
+Son engagement illustre une conviction simple :
+la technologie n’a de sens que si elle renforce
+la solidarité et la résilience collective.
+`;
+
+let index = 0;
+
+function typeEffect() {
+    if (index < content.length) {
+        hackerText.textContent += content.charAt(index);
+        index++;
+        setTimeout(typeEffect, 25); // vitesse d’écriture
+    }
+}
+
+typeEffect();
+
+
+
+/* ---------------------------------------
+   2) Animation des images de la GALERIE
+-----------------------------------------*/
 const galleryImages = document.querySelectorAll('.gallery img');
 
 window.addEventListener('scroll', () => {
